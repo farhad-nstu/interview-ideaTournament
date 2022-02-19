076ab3c58a373833956767400d24de011a5ff121
farhad-nstu <amfarhad33@gmail.com>
2022-02-19 11:05:30 +0600

    foreign key added

--- database/migrations/2022_02_19_050358_create_ideas_table.php
+++ database/migrations/2022_02_19_050358_create_ideas_table.php
@@ -17,9 +17,11 @@ class CreateIdeasTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->bigInteger('user_id')->unsigned();
             $table->text('idea');
             $table->integer('status')->default(1);
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
